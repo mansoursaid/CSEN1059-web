c517da318790f97b7452652cb8cfb68cc8baed22
silvermist <salma.khaled.e@gmail.com>
2016-04-02 22:41:59 +0200

    adding temp views

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Project;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Input;
 use App\Http\Requests;
 
 class ProjectsController extends Controller
