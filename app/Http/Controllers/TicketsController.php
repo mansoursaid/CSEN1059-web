99dc57cc5a2d71565df707a9532f399fe1f2fe7c
silvermist <salma.khaled.e@gmail.com>
2016-04-02 12:59:42 +0200

    adding store function

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Ticket;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -13,7 +14,33 @@ class TicketsController extends Controller
         return $tickets;
     }
 
-   
+    public function show($id){
+        $ticket = Ticket::find($id);
+        return $ticket;
+    }
+
+    /*public function create(){
+
+    }*/
+
+    public function store(Request $request){
+        $rules = array(
+                'tweet_id' => 'required',
+                'premium'  => 'required',
+                'urgency'  => 'required'
+            );
+
+        $validator = Validator::make(Input::all(), $rules);
+
+        if (!$validator->fails()) {
+            $ticket = new Ticket;
+            $ticket->tweet_id = Input::get('tweet_id');
+            $ticket->premium    = Input::get('premium');
+            $ticket->urgency = Input::get('urgency');
+            $ticket->save();
+        }
+
+    }
 
 
 }
