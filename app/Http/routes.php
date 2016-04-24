08e897d7479b0411490d78bbeb765d293bb06491
Lin <linsara1@gmail.com>
2016-04-24 13:16:13 +0200

    modified routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,36 +11,33 @@
 |
 */
 
+/**
+*
+*   Route::resource('users', 'UsersController');
+*
+*   Gives you these named routes:
+*
+*       Verb    Path                        Action  Route Name
+*       GET     /users                      index   users.index
+*       GET     /users/create               create  users.create
+*       POST    /users                      store   users.store
+*       GET     /users/{user}               show    users.show
+*       GET     /users/{user}/edit          edit    users.edit
+*       PUT     /users/{user}               update  users.update
+*       DELETE  /users/{user}               destroy users.destroy
+*
+*   The generated routes can be checked using 'php artisan route:list' command
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tickets','TicketsController@index');
-Route::get('tickets/create','TicketsController@create');
-Route::get('tickets/{id}','TicketsController@show');
-Route::post('tickets/store','TicketsController@store');
-Route::delete('tickets/{id}','TicketsController@destroy');
-Route::get('tickets/{id}/edit','TicketsController@edit');
-Route::post('tickets/{id}/edited','TicketsController@update');
-
-Route::get('projects','ProjectsController@index');
-Route::get('projects/create','ProjectsController@create');
-Route::get('projects/{id}','ProjectsController@show');
-Route::post('projects/store','ProjectsController@store');
-Route::delete('projects/{id}','ProjectsController@destroy');
-Route::get('projects/{id}/edit','ProjectsController@edit');
-Route::post('projects/{id}/edited','ProjectsController@update');
+Route::resource('tickets', 'TicketsController');
+Route::resource('projects', 'ProjectsController');
+Route::resource('users', 'UsersController');
 
 Route::get('mentions', 'TweetsController@index');
-
-Route::get('users','UsersController@index');
-Route::get('users/create','UsersController@create');
-Route::post('users','UsersController@store');
-Route::get('users/{id}','UsersController@show');
-Route::delete('users/{id}','UsersController@destroy');
-Route::get('users/{id}/edit','UsersController@edit');
-Route::PATCH('users/{id}','UsersController@update');
-Route::DELETE('users','UsersController@delete');
 
 Route::get('admin', function () {
     return view('admin_template');
@@ -53,8 +50,6 @@ Route::controllers([
 
 Route::get('/paypal', 'GenLinkPaypalController@handleTransaction');
 Route::get('/genlink', 'GenLinkPaypalController@generateLink');
-
-
 
 
 Route::get('/mail', function() {
@@ -78,8 +73,6 @@ Route::post('change_paypal_client_id', 'AppSettingsController@changePaypalClient
 Route::post('change_paypal_secret_key', 'AppSettingsController@changePaypalSecretKey');
 
 
-
-
 Route::get('fire', function () {
     // this fires the event
     event(new App\Events\NotificationsEvent());
@@ -89,6 +82,3 @@ Route::get('fire', function () {
 
 Route::resource('notifications', 'NotificationsController',
     ['only' => ['index']]);
-
-
-
