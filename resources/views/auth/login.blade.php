67e51fc9b06ae5c1b058f56a68c4c7ec117fe436
Lin <linsara1@gmail.com>
2016-04-24 14:41:30 +0200

    separated views files & added login.blade.php view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,55 @@
 <!-- resources/views/auth/login.blade.php -->
 
-<form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
+<!DOCTYPE html>
+<html>
+    @include('_head')
+    <body class="skin-blue">
+        <div class="wrapper">
+            <header class="main-header" id="guest-header"> </header>
+<!-- Content -->
+                <div class="content-wrapper" style="margin-left:0; height: 94vh;">
+                    <section class="content-header"></section>
+                    <section class="content">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="box box-info">
+                                <div class="box-header with-border">
+                                    <h3 class="box-title">Sign in</h3>
+                                </div>
+                                <form class="form-horizontal" action="home.html">
+                                    <div class="box-body">
+                                        <div class="form-group">
+                                            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+                                            <div class="col-sm-10">
+                                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
 
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
-
-    <div>
-        <button type="submit">Login</button>
-    </div>
-</form>
+                                            <div class="col-sm-10">
+                                                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-2 col-sm-10">
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox"> Remember me
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-footer">
+                                        <button type="submit" class="btn btn-info pull-right">Sign in</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </section><!-- /.content -->
+                </div><!-- /.content-wrapper -->
+            <footer class="main-footer" id="footer" style="margin-left:0;"></footer>
+        </div><!-- ./wrapper -->
+    </body>
+</html>
