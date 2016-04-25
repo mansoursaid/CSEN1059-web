a784502ce1baa6aef434046c9b062979b517e969
silvermist <salma.khaled.e@gmail.com>
2016-04-25 13:18:55 +0200

    add_users_to_project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Project;
+use App\User;
+use App\Project_User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use App\Http\Requests;
@@ -47,6 +49,38 @@ class ProjectsController extends Controller
         $project->name = Input::get('name');
         $project->save();
         return $project;
+    }
+
+    public function addUserToProject($id , $pid) {
+       
+        if(User::find($id)) {
+            $project_user = new Project_User;
+            $project_user->user_id = $id;
+            $project_user->project_id = $pid;
+            $project_user->save();
+            return $project_user;
+        }else{
+            //error message
+            echo 'errorrrr';
+        }
+
+    }
+
+    public function addUsersToProject($users, $pid){
+        if(Project::find($pid)){
+            foreach ($users as $user){
+                $id = $user->id;
+                if(Project_User::where('user_id', $id )->where('project_id',$pid)->get()!=null) {
+                    $this->addUserToProject($id, $pid);
+                }else{
+                    echo 'already exists';
+                }
+            }
+
+        }else{
+            echo 'errrorrrr';
+        }
+
     }
 
 }
