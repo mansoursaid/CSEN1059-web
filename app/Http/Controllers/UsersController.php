75dc2ab137c9ab708d61282dfcdc52e2a5d123cc
Lin <linsara1@gmail.com>
2016-05-02 22:42:21 +0200

    added auth middleware to user controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,6 +15,7 @@ class UsersController extends Controller
 
 
     public function __construct() {
+        $this->middleware('auth', ['except' => ['create']]);
         $this->middleware('isAdmin', ['only' => ['usersAddAndIndex', 'destroy', 'edit', 'update']]);
     }
 
