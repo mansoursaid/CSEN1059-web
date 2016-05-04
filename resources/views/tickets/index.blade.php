e00a3568ebb8b6960b01abf6826395582aa5916a
silvermist <salma.khaled.e@gmail.com>
2016-05-04 18:57:31 +0200

    fix bug

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -170,19 +170,15 @@
                                                     <span class="caret"></span>
                                                     <span class="sr-only">Toggle Dropdown</span>
                                                 </button>
-                                            
                                                 <ul class="dropdown-menu" role="menu">
-                                                    <li><a href="#" class="inprog">in progress</a></li>
-                                                    <li><a href="#" class="closed">Closed</a></li>
-                                                    <li><a href="#" class="open">Open</a></li>
+                                                    <li><a href="#" class="inprog" val="{{$ticket->id}}">in progress</a></li>
+                                                    <li><a href="#" class="closed" val="{{$ticket->id}}">Closed</a></li>
+                                                    <li><a href="#" class="open" val="{{$ticket->id}}">Open</a></li>
                                                 </ul>
                                             </div>
                                         </td>
                                     @endforeach
                                     </tbody>
-                                    <tfoot>
-                                    
-                                    </tfoot>
                                 </table></div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0">1</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0">2</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0">3</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0">4</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0">5</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0">6</a></li><li class="paginate_button next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0">Next</a></li></ul></div></div></div></div>
                 </div>
                 <!-- /.box-body -->
@@ -190,7 +186,7 @@
         </div>
     </div>
 @if(isset($ticket))
-<form  class="myForm" action="/tickets/{{$ticket->id}}" method="post" style="display:none">
+<form  class="myForm" action="" method="post" style="display:none">
     {!! method_field('patch') !!}
     <input type="hidden" name="_token" value="{{ csrf_token()}}"/>
     <input class="val" type="text" name="status"><br><br>
@@ -202,17 +198,33 @@
 
 @section('custom_scripts')
 <script type="text/javascript">
+
 $(function () {
-    $("a.closed").click(function(){
+
+    var $form_id;
+    var $form_action;
+
+    $(document).on('click', 'a.closed', function(){
        $(".val").val('2');
+       $form_id = $(this).attr("val");
+       $form_action = "/tickets/" + $form_id;
+       $(".myForm").attr("action", $form_action);
        $(".myForm").submit();
     });
+
     $("a.inprog").click(function(){
        $(".val").val('1');
+       $form_id = $(this).attr("val");
+       $form_action = "/tickets/" + $form_id;
+       $(".myForm").attr("action", $form_action);
        $(".myForm").submit();
     });
+
     $("a.open").click(function(){
        $(".val").val('0');
+       $form_id = $(this).attr("val");
+       $form_action = "/tickets/" + $form_id;
+       $(".myForm").attr("action", $form_action);
        $(".myForm").submit();
     });
 });
